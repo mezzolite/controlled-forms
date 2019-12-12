950ef43c1dc2962c997ffc0a75387130dfb32506
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-12-12 11:30:34 -0700

    Removed solution code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Form from './components/Form'
 import AdoptableDogs from './components/AdoptableDogs'
 const BASE_URL = `https://dogs-backend.herokuapp.com/dogs`
 class App extends Component {
@@ -14,28 +13,13 @@ class App extends Component {
       .then(adoptableDogs => this.setState({ adoptableDogs }))
   }
 
-  addDog = dog => {
-    fetch(BASE_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(dog),
-    }).then(response => response.json())
-      .then(dog => {
-        this.setState({
-          adoptableDogs: [...this.state.adoptableDogs, dog]
-        })
-      })
-  }
-
   render() {
     return (
       <div className="App" >
         <AdoptableDogs
           adoptableDogs={this.state.adoptableDogs}
         />
-        <Form addDog={this.addDog} />
+
       </div>
     );
   }
